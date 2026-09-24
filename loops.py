fada9fffb5fe97eb561ff06e6a3df3b5461f399a
audreyn14 <audreyn14@nycstudents.net>
2026-09-24 07:06:41 -0700

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,6 +1,9 @@
 import turtle
 from turtle import *
 t = Turtle()
+t.speed(0)
+
+
 
                 
 """ for i in range(60):
@@ -10,23 +13,36 @@ t = Turtle()
     t.right(5) """
 
 
+
+
 """ def square(length, angle):
-    for _ in range(4):
-        t.forward(length)
-        t.right(angle)
-
-def addSquares(iRange):
-    length = 25
-    for i in range(iRange):
-        square(length, 90)
-        length += 25
-addSquares(5)
- """
-
-
-for i in range(60):
-    for j in range(4):
-        t.forward(5)
-        t.right(90)
+    for i in range(4):
+        for j in range(4):
+            t.forward(length)
+            t.right(angle)
     t.right(5)
 
+def addSquares(iRange):
+    length = 5
+    for i in range(iRange):
+        square(length, 90)
+        length += 5
+addSquares(60) """
+
+
+
+def star(length, angle):
+    for i in range(5):
+        for j in range(5):
+            t.forward(length)
+            t.right(144)
+    t.right(5)
+
+def starspiral(iRange):
+    length = 5
+    for i in range(iRange):
+        star(length, 144)
+        length += 5
+starspiral(60)
+
+turtle.done()
